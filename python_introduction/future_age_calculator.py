f64a041c95454cb5428abc06c72ed6e70986fc7e
jalloh19 <ikdiallotechie@gmail.com>
2025-07-12 03:52:37 +0800

    Add the python_introduction folder

---
+++ python_introduction/future_age_calculator.py
@@ -0,0 +1,12 @@
+
+current_age_str = input("How old are you? ")
+
+#
+current_age = int(current_age_str)
+
+# Calculate the age difference between 2050 and 2023.
+years_to_add = 2050 - 2023
+
+# Calculate and Print
+future_age = current_age + years_to_add
+print(f"In 2050, you will be {future_age} years old.")
---
+++ python_introduction/hours_to_seconds.py
@@ -0,0 +1,12 @@
+
+# Define the variable for the number of hours
+hours = 2
+
+# Define the conversion factor
+seconds_in_an_hour = 3600
+
+# Calculate the total number of seconds
+seconds = hours * seconds_in_an_hour
+
+# Print the result in the specified format
+print(f"{hours} hour(s) is {seconds} seconds.")
